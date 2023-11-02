fc133d1da5e9c0d7a85a3e4cdba680c3fd68e265
kai <kai.couts@gmail.com>
2023-11-03 03:20:58 +0900

    Init commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/tokyonight.nvim",
+  opts = function()
+    return {
+      style = "moon",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    }
+  end
+}
---
+++ lua/plugins/nvim-neo-tree.lua
@@ -0,0 +1,106 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = function()
+    return {
+      sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+      open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+        window = {
+          mappings = {
+            ["<bs>"] = "navigate_up",
+            ["."] = "set_root",
+            ["H"] = "toggle_hidden",
+            ["/"] = "fuzzy_finder",
+            ["D"] = "fuzzy_finder_directory",
+            ["#"] = "fuzzy_sorter", -- fuzzy sorting using the fzy algorithm
+            -- ["D"] = "fuzzy_sorter_directory",
+            ["f"] = "filter_on_submit",
+            ["<c-x>"] = "clear_filter",
+            ["[g"] = "prev_git_modified",
+            ["]g"] = "next_git_modified",
+            ["O"] = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "o" } },
+            ["Oc"] = { "order_by_created", nowait = false },
+            ["Od"] = { "order_by_diagnostics", nowait = false },
+            ["Og"] = { "order_by_git_status", nowait = false },
+            ["Om"] = { "order_by_modified", nowait = false },
+            ["On"] = { "order_by_name", nowait = false },
+            ["Os"] = { "order_by_size", nowait = false },
+            ["Ot"] = { "order_by_type", nowait = false },
+          },
+          fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
+            ["<down>"] = "move_cursor_down",
+            ["<C-n>"] = "move_cursor_down",
+            ["<up>"] = "move_cursor_up",
+            ["<C-p>"] = "move_cursor_up",
+          },
+        },
+      },
+      window = {
+        mappings = {
+          ["<space>"] = {
+            "toggle_node",
+            nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
+          },
+          ["<2-LeftMouse>"] = "open",
+          ["o"] = "open",
+          ["<esc>"] = "cancel", -- close preview or floating neo-tree window
+          ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
+          -- Read `# Preview Mode` for more information
+          ["l"] = "focus_preview",
+          ["S"] = "open_split",
+          ["s"] = "open_vsplit",
+          -- ["S"] = "split_with_window_picker",
+          -- ["s"] = "vsplit_with_window_picker",
+          ["t"] = "open_tabnew",
+          -- ["<cr>"] = "open_drop",
+          -- ["t"] = "open_tab_drop",
+          ["w"] = "open_with_window_picker",
+          --["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
+          ["C"] = "close_node",
+          -- ['C'] = 'close_all_subnodes',
+          ["z"] = "close_all_nodes",
+          --["Z"] = "expand_all_nodes",
+          ["a"] = {
+            "add",
+            -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
+            -- some commands may take optional config options, see `:h neo-tree-mappings` for details
+            config = {
+              show_path = "none" -- "none", "relative", "absolute"
+            }
+          },
+          ["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+          ["d"] = "delete",
+          ["r"] = "rename",
+          ["y"] = "copy_to_clipboard",
+          ["x"] = "cut_to_clipboard",
+          ["p"] = "paste_from_clipboard",
+          ["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
+          -- ["c"] = {
+          --  "copy",
+          --  config = {
+          --    show_path = "none" -- "none", "relative", "absolute"
+          --  }
+          --}
+          ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
+          ["q"] = "close_window",
+          ["R"] = "refresh",
+          ["?"] = "show_help",
+          ["<"] = "prev_source",
+          [">"] = "next_source",
+          ["i"] = "show_file_details",
+        }
+      },
+      default_component_configs = {
+        indent = {
+          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+          expander_collapsed = "",
+          expander_expanded = "",
+          expander_highlight = "NeoTreeExpander",
+        },
+      },
+    }
+  end,
+}
